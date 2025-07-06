6c307c25b5d51db96cd959e9e9b787aa56fba00e
lori314 <153082618+lori314@users.noreply.github.com>
2025-07-06 13:12:05 +0800

    Add files via upload

--- create_samples.py
+++ create_samples.py
@@ -9,14 +9,19 @@ def main():
         os.makedirs("cleaned_data_samples")
     
     current_lines = 0
-    with open(SOURCE_CHUNK_PATH,"r",encoding='utf-8') as inputfile:
-        with open(TARGET_CHUNK_PATH,"w",encoding='utf-8') as outputfile:
-            while current_lines < LINES_LIMIT:
-                clean_text = inputfile.readline().strip()
-                outputfile.write(clean_text + '\n')
-                current_lines += 1
-    
-    print("1000条已样例生成，保存在cleaned_data_samples目录下")
+    try:
+        with open(SOURCE_CHUNK_PATH,"r",encoding='utf-8') as inputfile:
+            with open(TARGET_CHUNK_PATH,"w",encoding='utf-8') as outputfile:
+                while current_lines < LINES_LIMIT:
+                    clean_text = inputfile.readline().strip()
+                    outputfile.write(clean_text + '\n')
+                    current_lines += 1
+        
+        print("1000条已样例生成，保存在cleaned_data_samples目录下")
+
+    except FileNotFoundError:
+        print("样例提取失败")
+        print("请确保你已成功运行主清洗脚本，并且路径正确")
             
 if __name__ == "__main__":
     main()
