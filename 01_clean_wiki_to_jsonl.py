9c7685606b09a1a35689a7bd94dcd7adea93a151
lori314 <153082618+lori314@users.noreply.github.com>
2025-07-06 16:30:46 +0800

    Add files via upload

--- 01_clean_wiki_to_jsonl.py
+++ 01_clean_wiki_to_jsonl.py
@@ -1,16 +1,41 @@
 import json
 import os
 import glob
+import re
 from tqdm import tqdm
 
+#设置每个文件块的大小为20M
 CHUNK_SIZE_BYTES = 20*1024*1024
 
+def clean_dirty_text(text):
+    """
+    数据清洗函数，这里是一个比较简单的实现，可以根据实际需求增加数据筛选逻辑
+
+    Args:
+    text(str): 待清洗的字符串
+
+    Return:
+    str: 处理后的字符串
+    """
+    text = re.sub(r'<.*?>', '', text)
+    return text
+
+
 def clean_data(dirty_text_stream):
+    """
+    一个生成器，用来从脏的数据中提取关键信息
+    
+    Args:
+    dirty_text_stream(iterable): 一个可迭代对象，每一个对象都是一个json格式的字符串
+    
+    Yields:
+    str: 一个重新打包后的json格式的字符串，包含训练预处理所需信息并符合格式要求
+    """
     for dirty_text in dirty_text_stream:
         dirty_text_dict = json.loads(dirty_text)
         clean_text_dict = {}
-        if "text" in dirty_text_dict:
-            clean_text_dict["text"] = dirty_text_dict["text"]
+        if "text" in dirty_text_dict and len(dirty_text_dict["text"])!=0:
+            clean_text_dict["text"] = clean_dirty_text(dirty_text_dict["text"])
         else:
             continue
         meta = {}
@@ -21,10 +46,16 @@ def clean_data(dirty_text_stream):
         if "url" in dirty_text_dict:
             meta["url"] = dirty_text_dict["url"]
         clean_text_dict["meta"] = meta
-        clean_text = json.dumps(clean_text_dict,ensure_ascii=False,indent=4)
+        clean_text = json.dumps(clean_text_dict,ensure_ascii=False)
         yield clean_text
 
 def extracted_wiki():
+    """
+    一个生成器，用来将下载并由wikiextractor处理后的dumps中的json格式字符串按行提取
+
+    Yields:
+    str: 一个json格式字符串，按utf-8格式编码，即处理后的dumps中读取到的行
+    """
     input_dir = "extracted_test"
     file_paths = glob.glob(os.path.join(input_dir, '**', 'wiki_*'), recursive=True)
 
@@ -34,6 +65,9 @@ def extracted_wiki():
                 yield line
 
 def main():
+    """
+    主函数，处理目录下的数据并按jsonl格式分块存储
+    """
     dirty_text_stream = extracted_wiki()
     clean_text_stream = clean_data(dirty_text_stream)
 
@@ -45,6 +79,7 @@ def main():
         os.makedirs("output_dir")
 
     dir_name = "output_dir"
+    #这里要对文件分块存储，以防止单个文件过大而不方便使用
     for clean_text in clean_text_stream:
         if current_outputfile == None or current_chunk_size > CHUNK_SIZE_BYTES:
             if current_outputfile != None:
