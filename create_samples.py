d53e7ab82c180bec2423e78a69cee74c6ae1bb61
lori314 <153082618+lori314@users.noreply.github.com>
2025-07-06 13:04:45 +0800

    Add files via upload

---
+++ create_samples.py
@@ -0,0 +1,22 @@
+import os
+
+SOURCE_CHUNK_PATH = "output_dir/outputfile0.jsonl"
+TARGET_CHUNK_PATH = "cleaned_data_samples/samples.jsonl"
+LINES_LIMIT = 1000
+
+def main():
+    if not os.path.exists("cleaned_data_samples"):
+        os.makedirs("cleaned_data_samples")
+    
+    current_lines = 0
+    with open(SOURCE_CHUNK_PATH,"r",encoding='utf-8') as inputfile:
+        with open(TARGET_CHUNK_PATH,"w",encoding='utf-8') as outputfile:
+            while current_lines < LINES_LIMIT:
+                clean_text = inputfile.readline().strip()
+                outputfile.write(clean_text + '\n')
+                current_lines += 1
+    
+    print("1000条已样例生成，保存在cleaned_data_samples目录下")
+            
+if __name__ == "__main__":
+    main()
